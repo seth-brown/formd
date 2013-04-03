a5618731109d04743ed9c649051fb9a1983656bc
Seth Brown <sethbrown@drbunsen.org>
2013-04-03 06:49:02 -0400

    Added tests.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -4,45 +4,40 @@ sys.path.append('..')
 from formd import ForMd
 
 class TestForMd(unittest.TestCase):
+    no_md = ''
     ref = '[Markdown][1]\n\n\n[1]: http://en.wikipedia.com/wiki/Markdown'
     inl = '[Markdown](http://en.wikipedia.com/wiki/Markdown)'
-    txt = '\n\n'.join(('I like this [pangram][10]:',
-          'Quick wafting zephyrs vex bold Jim[^fn2]',
-          'This one is exactly 26 letters:',
-          'Blowzy night-frumps vex\'d Jack Q.[^fn1]',
-          '[^fn1]: This is a perfect one!',
-          '[^fn2]: This is not a perfect pangram\n',
-          '[10]: http://en.wikipedia.org/wiki/Pangram'))
+
+    def test_none(self):
+        f = ForMd(self.no_md)
+        flip_conv = f.flip()
+        f = ForMd(flip_conv)
+        flip_conv = f.flip()
+        self.assertEqual(self.no_md, flip_conv)
 
     def test_ref(self):
         f = ForMd(self.inl)
-        ref_conv = f.ref_md()
+        ref_conv = list(f.ref_md())[0]
         self.assertEqual(self.ref, ref_conv)
 
     def test_inl(self):
         f = ForMd(self.ref)
-        inline_conv = f.inline_md()
+        inline_conv = list(f.inline_md())[0]
         self.assertEqual(self.inl, inline_conv)
 
-    def test_crossinl(self):
-        """ Format URLs spanning across line breaks
-        """
-        cross_inl = self.inl.replace(':', ':\n')
-        f = ForMd(cross_inl)
-        ref_conv = f.ref_md()
-        self.assertEqual(self.ref, ref_conv)
+    def test_flip(self):
+        f = ForMd(self.ref)
+        inline_conv = list(f.inline_md())[0]
+        self.assertEqual(self.inl, inline_conv)
 
-    def test_fancy(self):
-        txt = '\n\n'.join(('I like this [pangram][1]:',
-              'Quick wafting zephyrs vex bold Jim[^fn1]',
-              'This one is exactly 26 letters:',
-              'Blowzy night-frumps vex\'d Jack Q.[^fn2]',
-              '[^fn1]: This is not a perfect pangram',
-              '[^fn2]: This is a perfect one!\n',
-              '[1]: http://en.wikipedia.org/wiki/Pangram'))
-        f = ForMd(self.txt)
-        formd_txt = f.ref_md()
-        self.assertEqual(txt, formd_txt)
+    def test_break_url(self):
+        """ Format URLs spanning across a line break
+        """
+        cross_inl = self.inl.replace('http:', 'http:\n')
+        cross_ref = self.ref.replace('http:', 'http:\n')
+        f = ForMd(cross_inl)
+        ref_conv = list(f.ref_md())[0]
+        self.assertEqual(cross_ref, ref_conv)
 
 
 if __name__ == '__main__':
