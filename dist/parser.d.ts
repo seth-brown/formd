380444249cdbd99a8bfe056de8eeab4ebd549018
Seth Brown <software@seth-brown.net>
2020-04-25 21:09:29 -0400

    untracked dist

--- dist/parser.d.ts
+++
@@ -1,4 +0,0 @@
-import { Format, TokenArray, LinkArray, MarkdownArray } from './interfaces';
-export declare const processLinks: (links: LinkArray) => Promise<MarkdownArray>;
-export declare const parser: (md: any, tokens: TokenArray, format: Format) => Promise<any>;
-export declare const app: (md: any, format: Format) => Promise<void>;
